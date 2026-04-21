cc2f80a9ca1778834c2727cc044ce12e217f2945
Rishvanth SK <ceo.rish@gmail.com>
2026-04-21 22:44:26 +0530

    App ready to be deployed

--- src/main/java/com/example/demo/repository/CustomerClassificationRepo.java
+++ src/main/java/com/example/demo/repository/CustomerClassificationRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.CustomerClassificationType;
 
-public interface CustomerClassificationRepo extends JpaRepository<CustomerClassificationType,Long> {
-	
+public interface CustomerClassificationRepo extends JpaRepository<CustomerClassificationType,Long> {	
+
 }
